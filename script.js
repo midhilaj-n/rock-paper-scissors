f81cf055ca7ab6587f4f1b3f5535fec3f194814b
midhilaj-n <midhilajnisam1@gmail.com>
2024-09-27 11:00:23 +0530

    Created a function to play the game

--- script.js
+++ script.js
@@ -3,20 +3,76 @@ let computerScore = 0;
 
 const getComputerChoice = () => {
     const computerChoice =  Math.round(Math.random() * 2);
+    let result;
     if(computerChoice === 0){
-        console.log("Computer: Rock");
+        result = `Rock`
 
     }else if(computerChoice === 1){
-        console.log("Computer: Paper");
+        result = `Paper`;
     }else{
-        console.log("Computer: Scissor");
+        result = `Scissor`;
     }
+
+    return result.toLowerCase();
 }
 
-const getHumanChoice = (choice) => {
-    choice = prompt("Rock. Paper. Scissor?");
-    console.log(`You: ${choice}`)
+const getHumanChoice = () => {
+    const choice = prompt("Rock. Paper. Scissor?");
+    return `You: ${choice}`;
 
 }
-getHumanChoice()
-getComputerChoice()
+
+
+
+
+
+const playRound = (humanChoice, computerChoice) => {
+    const lowerCase = humanChoice.toLowerCase();
+    let result;
+    if(humanChoice === computerChoice){
+        result = `Tie.`
+    }
+    
+    else if(lowerCase === 'rock' && computerChoice === 'paper'){
+        computerScore++;
+        result = `Computer wins. Paper beats Rock.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }
+    
+    else if(lowerCase === 'rock' && computerChoice === 'scissor'){
+        humanScore++;
+        result = `You wins. Rock beats Scissor.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }
+    
+    else if(lowerCase === 'paper' && computerChoice == 'rock'){
+        humanScore++;
+        result = `You wins. Paper beats Rock.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }
+    
+    else if(lowerCase === 'paper' && computerChoice === 'scissor'){
+        computerScore++;
+        result = `Computer wins. Scissor beats Paper.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }
+
+    else if(lowerCase === 'scissor' && computerChoice === 'rock'){
+        computerScore++
+        result = `Computer wins. Rock beats Scissor.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }
+
+    else if(lowerCase === 'scissor' && computerChoice === 'paper'){
+        humanScore++
+        result = `You wins. Scissor beats Paper.
+        Score: You: ${humanScore} & Computer: ${computerScore}`
+    }else{
+        result = 'Invalid result try again with correct input!'
+    }
+
+    return result;
+
+
+}
+
